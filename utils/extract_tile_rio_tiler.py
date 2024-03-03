0386d3d870ff23d7d329eb9fe53656f2a75b60eb
Julien Rebetez <julien@fhtagn.net>
2024-03-03 07:48:42 +0100

    Make extract_rio_tiler compatible with extract_tile

--- utils/extract_tile_rio_tiler.py
+++ utils/extract_tile_rio_tiler.py
@@ -4,21 +4,21 @@ Utility to compare to XYZ tiles extracted by rio-tiler
 import argparse
 from rio_tiler.io import Reader
 import pylab as pl
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="compare_to_rio_tiler"
     )
     parser.add_argument("filename")
-    parser.add_argument("tile_x", type=int)
-    parser.add_argument("tile_y", type=int)
-    parser.add_argument("tile_z", type=int)
+    parser.add_argument("z", type=int)
+    parser.add_argument("x", type=int)
+    parser.add_argument("y", type=int)
 
     args = parser.parse_args()
 
     with Reader(args.filename) as image:
-        tile = image.tile(args.tile_x, args.tile_y, args.tile_z)
+        tile = image.tile(args.x, args.y, args.z)
         img = tile.data.transpose(1, 2, 0)
         # tile also has a .mask that we should compare against
-    pl.imshow(img)
-    pl.show()
+    np.save('rio_tile.npy', img)
