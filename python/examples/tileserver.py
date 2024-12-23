ac83cc96d289be3525669e221238aa32c6af861c
Julien Rebetez <julien@fhtagn.net>
2024-12-23 14:51:05 +0100

    Python example tileserver: add directory support

--- python/examples/tileserver.py
+++ python/examples/tileserver.py
@@ -1,13 +1,22 @@
 """
 Example of implementing a tileserver in python
+
+You can either navigate to localhost:8080 and put a filename to visualize tiles served for it.
+
+Or you can run this with -d to index .tif files in a given directory and then navigate to
+localhost:8080/examples to see a list of those tif, with a direct link to the visualization
 """
 
+import os
 import acog
 import json
+import argparse
 from aiohttp import web
 from PIL import Image
 import numpy as np
+import urllib.parse
 from io import BytesIO
+import functools
 
 INDEX_HTML = """
 <head>
@@ -77,6 +86,7 @@ INDEX_HTML = """
         </div>
     </div>
     <script>
+        const queryParams = new URLSearchParams(window.location.search);
         const map = L.map('map').setView([0, 0], 4);
         const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -84,6 +94,12 @@ INDEX_HTML = """
         }).addTo(map);
 
         let tileLayer = null;
+
+        if (queryParams.get('filename') !== undefined) {
+            const input = document.getElementById('filename');
+            input.value = queryParams.get('filename')
+            // TODO: Same for params?
+        }
 
         function exampleSelected () {
             const example = document.getElementById('example').value;
@@ -99,7 +115,9 @@ INDEX_HTML = """
             const filename = input.value;
             const params = document.getElementById('params').value;
 
+            console.log('before');
             const resp = await fetch("/bounds/" + filename);
+            console.log('after', resp);
             if (!resp.ok) {
                 throw new Error(`Response status ${resp.status}`);
             }
@@ -166,15 +184,56 @@ async def tile(request):
     return web.Response(body=buffer.getvalue(), content_type="image/png")
 
 
-app = web.Application()
-app.add_routes(
-    [
-        web.get("/", index),
-        web.get(r"/tile/{filename:.+}/{z}/{x}/{y}", tile),
-        web.get(r"/bounds/{filename:.+}", bounds),
-    ]
-)
+async def examples(request, example_files):
+    html = ""
+    for file in example_files:
+        html += (
+            f'<a href="/?filename={urllib.parse.quote_plus(file)}">'
+            f"{os.path.basename(file)}</a><br />\n"
+        )
+    return web.Response(body=html, content_type="text/html")
+
+
+def list_tiffs_in_dir(directory: str):
+    if directory.startswith(".."):
+        raise ValueError("Can't use -d to point to a parent directory")
+
+    files = []
+    with os.scandir(directory) as it:
+        for entry in it:
+            if not (
+                entry.name.lower().endswith(".tiff")
+                or entry.name.lower().endswith(".tif")
+            ):
+                continue
+            files.append(os.path.join(directory, entry.name))
+    return files
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="acog-py-tileserver", description="acog demo python tileserver"
+    )
+    parser.add_argument(
+        "-d", "--directory", help="A local directory to list examples from"
+    )
+    args = parser.parse_args()
+
+    if args.directory is not None:
+        example_files = list_tiffs_in_dir(args.directory)
+    else:
+        example_files = []
+
+    app = web.Application()
+    app.add_routes(
+        [
+            web.get("/", index),
+            web.get(r"/tile/{filename:.+}/{z}/{x}/{y}", tile),
+            web.get(r"/bounds/{filename:.+}", bounds),
+            web.get(
+                "/examples/", functools.partial(examples, example_files=example_files)
+            ),
+        ]
+    )
+
     web.run_app(app)
